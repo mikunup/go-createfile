be26c569837e7b8d1d6dc9df87d8e5f8a3cf79ee
mikunup <lv.mitsuru@gmail.com>
2020-01-05 20:21:42 +0900

    modified Generator Patternの適応

--- main.go
+++ main.go
@@ -69,19 +69,30 @@ func initArgs() error {
 }
 
 func createFiles() error {
-	tt := term.NewTimeTerm(time.Now(), MovingDate)
-	for i := 0; i < CreateCount; i++ {
-		creatTime := tt.NextTerm()
-		name := fmt.Sprintf("%s.txt", creatTime.Format(FileNameLayout))
+	for date := range nextTerms(0, CreateCount) {
+		name := fmt.Sprintf("%s.txt", date.Format(FileNameLayout))
 		fp := filepath.Join(CreateFilePath, name)
 		file, err := os.OpenFile(fp, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			return fmt.Errorf("todo context :%s", err)
 		}
-		file.Close()
-		os.Chtimes(fp, creatTime, creatTime)
+		defer file.Close()
+		os.Chtimes(fp, date, date)
 	}
 	return nil
+}
+
+// nextTermsは次に生成するファイルのtime.Timeを返却する
+func nextTerms(start int, end int) <-chan time.Time {
+	tt := term.NewTimeTerm(time.Now(), MovingDate)
+	ch := make(chan time.Time)
+	go func(ch chan time.Time) {
+		for i := start; i <= end; i++ {
+			ch <- tt.NextTerm()
+		}
+		close(ch)
+	}(ch)
+	return ch
 }
 
 func fileTermIs() bool {
